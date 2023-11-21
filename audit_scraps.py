db074c6d6653607a8182552eb6506af36c5131be
fayzu001 <fayzu001@umn.edu>
2023-11-21 09:58:28 -0600

    Made some modification to the script to obtain information on subjects with unique sessions id that has/has not anat data

--- audit_scraps.py
+++ audit_scraps.py
@@ -20,17 +20,15 @@ def main():
     subject_lists[DICOM2BIDS] = audit_abcd_dicom2bids(dicom_db)
 
 
-def get_dicom_db():
+def get_dicom_db(file_path):
     # TODO Make this function without hardcoding
-    return pd.read_csv(PATH_DICOM_DB)
+    return pd.read_csv(file_path)
 
 
 
 def audit_abcd_dicom2bids(dicom_db):
     # DICOM2BIDS FLOW
-    bids_columns = ["bids_subject_id", "bids_session_id"]
-    out_paths = dict()
-
+    bids_columns = ["subject", "session"]
     sub_col = dicom_db.columns.values[0]  # "subject"
     ses_col = dicom_db.columns.values[1]  # "session"
     sessions=list(set(dicom_db[ses_col]))  # TODO Ensure that this gets the right column name (right now it's just "session")
@@ -40,51 +38,26 @@ def audit_abcd_dicom2bids(dicom_db):
     # Output: List of excluded subjects for that year, and BIDS DB with them excluded
     dicom_db_with_anat, dicom_db_no_anat = query_split_by_anat(dicom_db)
 
-    
-    # For each year, check/count every subject-session's BIDS conversion status
-    counts = dict()
-    ses_DBs = dict()
-
-    sub_ses_df_BIDS_NGDR = get_sub_ses_df_from_tier1(PATH_NGDR, sub_col,
-                                                     ses_col)
-
    
     for session in sessions:
-        counts[session] = dict()
-        ses_DBs[session] = dict()
-
-
         # Split dicoms_db by session and by whether BIDS conversion succeeded
-        session_db = dicom_db_with_anat.loc[dicom_db_with_anat["session"] == session]
-        ses_DBs[session] = {
-            "Total": query_has_anat(session_db),
-            "No-anat": dicom_db_no_anat[dicom_db_no_anat["session"] == session],
+        session_db = dicom_db_with_anat.loc[dicom_db_with_anat[ses_col] == session]
+        has_anat = query_has_anat(session_db)
+        no_anat = dicom_db_no_anat[dicom_db_no_anat[ses_col] == session]
 
-        }
+        # Save the session specific data to CSV files
+        file_path_has_anat = f'/home/rando149/shared/projects/begim_test/audit_year2_bids_count/{session}_has_anat.csv'
+        file_path_no_anat = f'/home/rando149/shared/projects/begim_test/audit_year2_bids_count/{session}_no_anat.csv'
 
-        for convert_status in ses_DBs[session].keys():
-            counts[session][convert_status] = len(ses_DBs[session][convert_status])
-            print("{} subjects in {}{}: {}"  # TODO Move the "print" functionality to audit count table flow section
-                  .format(convert_status, session,
-                          "" if convert_status[:2].lower() == "no" else " with anat",
-                          counts[session][convert_status]))
+        has_anat.to_csv(file_path_has_anat, index=False)
+        no_anat.to_csv(file_path_no_anat, index=False)
 
-            # Save lists of subject-sessions (1 per conversion status)
-            ses_DBs[session][convert_status].to_csv(
-                out_paths["uploads_subject_list_" + session
-                          ].format(convert_status.replace(" ", "_")),
-                columns=[sub_col, ses_col], index=False, header=bids_columns
-            )
+    # Saving additional data if required
+    all_sub_ses_NGDR = get_sub_ses_df_from_tier1(PATH_NGDR)
+    all_sub_ses_NGDR.to_csv('/home/rando149/shared/projects/begim_test/audit_year2_bids_count/NGDR_subjects.csv')
+
+    return {"has_anat": dicom_db_with_anat, "no_anat": dicom_db_no_anat}
    
-def make_has_anat_DICOM_db(dicom_db_with_anat):
-    dicom_db_with_anat.to_csv('has_anat_DICOMs.csv')
-    
-def make_no_anat_DICOM_db(dicom_db_no_anat):
-    dicom_db_no_anat.to_csv('no_anat_DICOMs.csv')
-    
-def make_NGDR_list_db(all_sub_ses_NGDR):
-    all_sub_ses_NGDR.to_csv('NGDR_subjects.csv')
-
 
 def query_split_by_anat(df):
     """
@@ -93,18 +66,18 @@ def query_split_by_anat(df):
     filter_cond = df.filter(regex='T[1,2].*').isin([np.nan]).all(axis=1)
     return df[~filter_cond], df[filter_cond]
 
-def get_sub_ses_df_from_tier1(tier1_dirpath, sub_col, ses_col):
+def get_sub_ses_df_from_tier1(tier1_dirpath):
     path_col = "tier1_dirpath"
 
     # Get DF of all subjects and their sessions in the NGDR space
-    all_sub_ses_NGDR_paths = glob(os.path.join(tier1_dirpath, "sub-*", "ses-*"))
+    all_sub_ses_NGDR_paths = glob.glob(os.path.join(tier1_dirpath, "sub-*", "ses-*"))
     # NOTE Later we can verify specific files existing instead of just the
     #      session directories existing
     all_sub_ses_NGDR = pd.DataFrame({path_col: [path for path in all_sub_ses_NGDR_paths]})
-    all_sub_ses_NGDR[sub_col] = all_sub_ses_NGDR[path_col].apply(
+    all_sub_ses_NGDR['subject'] = all_sub_ses_NGDR[path_col].apply(
         lambda path: os.path.basename(os.path.dirname(path))
     )
-    all_sub_ses_NGDR[ses_col] = all_sub_ses_NGDR[path_col].apply(
+    all_sub_ses_NGDR['session'] = all_sub_ses_NGDR[path_col].apply(
         lambda path: os.path.basename(path)
     )
     return all_sub_ses_NGDR
@@ -115,3 +88,6 @@ def query_has_anat(df):
     :param df: pandas.DataFrame
     """
     return df[~df.filter(regex='T[1,2].*').isin([np.nan]).all(axis=1)]
+
+if __name__ == "__main__":
+    main()
